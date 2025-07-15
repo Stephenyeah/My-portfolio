03e92269458a84c432bcdf52c9aeabe83d03e641
Stephen Yang <112423397+Stephenyeah@users.noreply.github.com>
2025-07-16 01:19:32 +0300

    correct the basepath

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-    ? process.env.NEXT_PUBLIC_BASE_PATH + '/'
-    : '/',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   distDir: 'out',
 };
 
